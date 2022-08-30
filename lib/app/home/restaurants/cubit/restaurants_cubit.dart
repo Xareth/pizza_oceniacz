60923f5999e357f994bd6ee76352b438f4d3c32a
Mikołaj Szadkowski <w6white@gmail.com>
2022-08-30 11:07:06 +0200

    Stwórz Cubit i State zamiast Streambuildera

--- lib/app/home/restaurants/cubit/restaurants_cubit.dart
+++ lib/app/home/restaurants/cubit/restaurants_cubit.dart
@@ -1,8 +1,12 @@
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
 
 part 'restaurants_state.dart';
 
 class RestaurantsCubit extends Cubit<RestaurantsState> {
-  RestaurantsCubit() : super(RestaurantsState());
+  RestaurantsCubit()
+      : super(
+          RestaurantsState(documents: [], isLoading: false, errorMessage: ''),
+        );
 }
--- lib/app/home/restaurants/restaurants_page_content.dart
+++ lib/app/home/restaurants/restaurants_page_content.dart
@@ -1,5 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'cubit/restaurants_cubit.dart';
 
 class RestaurantPageContent extends StatelessWidget {
   const RestaurantPageContent({
@@ -8,42 +11,50 @@ class RestaurantPageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-        stream: FirebaseFirestore.instance
-            .collection('restaurants')
-            .orderBy('rating', descending: true)
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Center(
-                child: Text('Error has occured: ${snapshot.error.toString()}'));
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: Text('Loading..'));
-          }
+    return BlocProvider(
+      create: (context) => RestaurantsCubit(),
+      child: BlocBuilder<RestaurantsCubit, RestaurantsState>(
+        builder: (context, state) {
+          return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+              stream: FirebaseFirestore.instance
+                  .collection('restaurants')
+                  .orderBy('rating', descending: true)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Center(
+                      child: Text(
+                          'Error has occured: ${snapshot.error.toString()}'));
+                }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: Text('Loading..'));
+                }
 
-          final documents = snapshot.data!.docs;
+                final documents = snapshot.data!.docs;
 
-          return ListView(children: [
-            for (final document in documents) ...[
-              Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(document['name']),
-                        Text(document['pizza']),
-                      ],
-                    ),
-                    Text(document['rating'].toString()),
-                  ],
-                ),
-              )
-            ]
-          ]);
-        });
+                return ListView(children: [
+                  for (final document in documents) ...[
+                    Padding(
+                      padding: const EdgeInsets.all(25.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(document['name']),
+                              Text(document['pizza']),
+                            ],
+                          ),
+                          Text(document['rating'].toString()),
+                        ],
+                      ),
+                    )
+                  ]
+                ]);
+              });
+        },
+      ),
+    );
   }
 }
